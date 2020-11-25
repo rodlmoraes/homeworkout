7d73b5f724f31f149cb7ea78f1f31ced8314537d
Rogério Ferreira Dos Santos <rogeroficial1506@gmail.com>
2020-11-25 20:52:33 -0300

    Alterações do método de cadastro, entregando a funcionalidade de nome e atualização dos testes

--- app/javascript/pages/SignUp.tsx
+++ app/javascript/pages/SignUp.tsx
@@ -1,5 +1,6 @@
 import { Card, createStyles, makeStyles, Typography } from '@material-ui/core'
 import React, { useState } from 'react'
+import api from '../services/api'
 
 import TextInput from '../components/TextInput'
 import { useAuth } from '../contexts/auth'
@@ -8,12 +9,11 @@ import SecretInput from '../components/SecretInput'
 import LargeButton from '../components/LargeButton'
 
 export default function SignUp() {
-  const { signUp } = useAuth()
-
-  const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
   const classes = useStyles()
+  const { signUp } = useAuth()
 
   return (
     <>
@@ -21,25 +21,34 @@ export default function SignUp() {
       <Card className={classes.cardSingUp}>
         <Typography variant='h3'>Cadastre-se e adicione novas aulas!</Typography>
         <TextInput
+          name='Nome'
+          label='Nome'
+          onChange= {e => { setName(e.target.value) }}
+          placeholder='Digite aqui seu nome'
+          value={name}
+        />
+        <TextInput
           name='Email'
           label='Email'
           onChange= {e => { setEmail(e.target.value) }}
           placeholder='Digite aqui seu email'
           value={email}
         />
-        <SecretInput
-          onChange= {e => { setPassword(e.target.value) }}
-          value={password}
-        />
-        <LargeButton
-          color='secondary'
-          onClick={() => signUp(email, password)}
-        >
-            Cadastrar
-        </LargeButton>
+        <SecretInput onChange= {e => { setPassword(e.target.value) }} value={password} />
+        <LargeButton color='secondary' onClick={() => {
+          signUp(email, password).then(() => putName(name))
+        }}>Cadastrar</LargeButton>
       </Card>
     </>
   )
+}
+const putName = async (name: string) => {
+  try {
+    await api.put('/current_teacher/0', { teacher: { name: name, email: null, image: null } },
+    )
+  } catch {
+    alert('Erro ao tentar atualizar informações!')
+  }
 }
 
 const useStyles = makeStyles(() =>
